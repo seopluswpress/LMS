91fb24cb7e9101d8e713230d40bd470729365163
seopluswpress <seopluswpress@gmail.com>
2025-10-16 12:26:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000', // allow this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true, // if you use cookies
+}));
 
 // ======================
 // MongoDB Models
